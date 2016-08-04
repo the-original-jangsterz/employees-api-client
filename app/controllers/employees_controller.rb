4efd4dd34eb437bf20496b4f5ef0abe691ad3a4b
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 18:34:37 -0500

    Add employee show action THE RIGHT WAY

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,6 @@
 class EmployeesController < ApplicationController
   def show
-    @employee = Employee.find_by(id: params[:id])
+    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
     render 'show.html.erb'
   end
 end
