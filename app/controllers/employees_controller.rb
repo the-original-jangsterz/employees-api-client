4cdf1a9d23436bb06614055ed79e2aaca0832290
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 19:20:08 -0500

    Add employee index action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,9 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+    render 'index.html.erb'
+  end
+
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
     render 'show.html.erb'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/employees' => 'employees#index'
   get '/employees/:id' => 'employees#show'
 end
