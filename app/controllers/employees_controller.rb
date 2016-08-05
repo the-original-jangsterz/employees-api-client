bf84436e1417fefec192639609e9ba637922f162
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 19:46:10 -0500

    Add employee new and create actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,6 +4,23 @@ class EmployeesController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @employee = Unirest.post(
+      "http://localhost:3000/api/v1/employees.json",
+      headers: { "Accept" => "application/json" },
+      parameters: {
+        first_name: params[:form_input_first_name],
+        last_name: params[:form_input_last_name],
+        email: params[:form_input_email]
+      }
+    ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
     render 'show.html.erb'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/employees' => 'employees#index'
+  get '/employees/new' => 'employees#new'
+  post '/employees' => 'employees#create'
   get '/employees/:id' => 'employees#show'
 end
